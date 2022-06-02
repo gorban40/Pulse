449e1135e6570b061d970ad69035bc7f2ec24df4
MishaGorban <mixei8189@gmail.com>
2022-06-02 20:42:40 +0300

    add_slider

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/watches/arrow/left_arrow.png"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/watches/arrow/right_arrow.png"</button>',
+      });
+});
