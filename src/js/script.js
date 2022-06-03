74c20faca28ef65cb597c1f94eff7de44ebf338d
MishaGorban <mixei8189@gmail.com>
2022-06-03 11:34:09 +0300

    add_catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,46 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.slider__inner').slick({
         speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icon/watches/arrow/left_arrow.png"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icon/watches/arrow/right_arrow.png"</button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/watches/arrow/left_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/watches/arrow/right_arrow.png"></button>',
       });
+}); */
+
+const slider = tns({
+  container: '.slider__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
 });
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
+
+(function($) {
+  $(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    function toggleSlide(item){
+      $(item).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+  });
+  })(jQuery);
